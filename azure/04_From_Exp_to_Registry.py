28fab4e8aa5739380820c8eeb614609968ad1a3c
kuzmin@hotmail.com <kuzmin@hotmail.com>
2022-01-20 23:53:30 +0000

    Added the legal blurb

--- azure/04_From_Exp_to_Registry.py
+++ azure/04_From_Exp_to_Registry.py
@@ -113,4 +113,8 @@ mlflow_call_endpoint('comments/create', 'POST', json.dumps(comment_body))
 
 # COMMAND ----------
 
-
+# MAGIC %md-sandbox
+# MAGIC &copy; 2022 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/>
+# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="http://help.databricks.com/">Support</a>
